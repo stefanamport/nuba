67f85439775ddfa75fba29f51dfb4e1032d7c404
stefanamport <stefan.amport@hsr.ch>
2016-12-04 14:19:57 +0100

    testscripts angepasst

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: '/app/header/header.html'
+  templateUrl: './app/app.component.html'
 })
 export class AppComponent  { name = 'Angular'; }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { AppHeader }   from './header/header.component';
 import { JournalComponent }   from './nubaJournal/nubaJournal.component';
 import { AnalysisComponent }   from './nubaAnalysis/nubaAnalysis.component';
 import { UserAccountComponent }  from './nubaUserAccount/nubaUserAccount.component';
@@ -11,7 +12,7 @@ import { AppRoutingModule }     from './routing/routing.module';
 
 @NgModule({
   imports:      [ BrowserModule, AppRoutingModule],
-  declarations: [ AppComponent, JournalComponent, AnalysisComponent, UserAccountComponent ],
+  declarations: [ AppComponent, AppHeader, JournalComponent, AnalysisComponent, UserAccountComponent ],
   bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
