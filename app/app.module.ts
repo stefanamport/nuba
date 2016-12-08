65b6dd937ee69157e39526a58931b94ca83ad1ac
Sonja Näf <sonja.naef@hsr.ch>
2016-12-08 11:44:14 +0100

    Merge new updates of master into firebase branch

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,19 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { AppHeader }   from './header/header.component';
-
 import { JournalComponent }   from './nubaJournal/nubaJournal.component';
-import { NubaSearch } from "./nubaJournal/nubaSearch.component";
-
 import { AnalysisComponent }   from './nubaAnalysis/nubaAnalysis.component';
 import { UserAccountComponent }  from './nubaUserAccount/nubaUserAccount.component';
+import { AngularFireModule } from 'angularfire2';
 
 
 import { AppRoutingModule }     from './routing/routing.module';
+import {NubaSearch} from "./nubaJournal/nubaSearch.component";
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBf7RiiafbN6IKzYoDdsZtOaQqFK-54oB0',
+  authDomain: "nuba-c3e84.firebaseapp.com",
+  databaseURL: 'https://nuba-c3e84.firebaseio.com/',
+  storageBucket: 'nuba-c3e84.appspot.com',
+  messagingSenderId: "126418702718"
+}
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule],
-  declarations: [ AppComponent, AppHeader, JournalComponent, NubaSearch, AnalysisComponent, UserAccountComponent ],
+  imports:      [ BrowserModule, AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig)],
+  declarations: [ AppComponent, AppHeader, JournalComponent, AnalysisComponent, UserAccountComponent, NubaSearch ],
   bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
