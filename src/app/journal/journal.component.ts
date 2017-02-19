82fe4ac7622a4ab66ffa7251c27893c34660ead2
Stefan Amport <privat@stefanamport.ch>
2017-02-19 12:00:07 +0100

    prepared coach functionality, mobile  coach as floating action button

--- src/app/journal/journal.component.ts
+++ src/app/journal/journal.component.ts
@@ -10,9 +10,43 @@ import { JournalEntriesService } from './journalEntries.service';
 })
 export class JournalComponent implements OnInit {
 
-  constructor() { }
+  mobCoachVisible = false;
+  newHintAvailable = false;
+  coachTip: any;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+
+  	// MOCK for showing functionalti
+  	// Shakes the Coach Box ;-)
+       this.coachTip = 'Du hast zu wenig Vitamine und Kohlenhydrate zu dir genommen. - Iss doch eine Banane.';
+       this.showNewHint();
+    // end mock
+
+  }
+
+  public mobCoachVisibility() {
+     this.mobCoachVisible = !this.mobCoachVisible;
+  }
+
+  public showNewHint() {
+
+  	// shakes Coach Box by CSS Animation :-)
+    this.switchShowNewHint();
+
+    var scope = this;
+
+    setTimeout(function(){
+       scope.switchShowNewHint();
+    }, 1000);
+
+  }
+
+  private switchShowNewHint(){
+    this.newHintAvailable = !this.newHintAvailable;
   }
 
 }
