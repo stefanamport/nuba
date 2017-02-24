001b305e5f292463646503eed065b8ff1bfe5520
Stefan Amport <privat@stefanamport.ch>
2017-02-24 21:58:13 +0100

    better form validation

--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -15,6 +15,7 @@ export class UserAccountComponent  {
   genders: any;
   activityLevels: any;
   formValidation: any = {};
+  savedMessage = '';
 
   constructor (private userService: UserService) {
     this.formValidation.valid = true;
@@ -36,21 +37,69 @@ export class UserAccountComponent  {
   validateForm(): boolean {
 
     this.formValidation.valid = true;
+    this.formValidation.messages = [];
 
      // check age
-     let age = this.userService.calcAge(this.user.birthday);
-
-     if (age < 18) {
-       this.formValidation.message = 'Zu jung... Leider können wir für unter 18-jährige keine zuverlässigen Berechnungen anbieten.';
-       this.formValidation.valid = false;
-     }
+     this.validateAge();
+     this.validateBodyweight();
+     this.validateBodyheight();
+     this.validateHoursOfSport();
 
      return this.formValidation.valid;
+  }
+
+  private validateBodyweight() {
+    let weight = this.user.bodyweight;
+
+    if (!weight || weight < 30 || weight > 200) {
+      this.formValidation.messages.push( 'Bitte trage dein korrektes Gewicht ein' );
+      this.formValidation.valid = false;
+    }
+  }
+
+  private validateBodyheight() {
+
+    let height = this.user.bodyheight;
+
+    if (!height || height < 60 || height > 200 ) {
+      this.formValidation.messages.push( 'Bitte trage deine korrekte Körpergrösse ein' );
+      this.formValidation.valid = false;
+    }
+
+  }
+
+  private validateHoursOfSport() {
+    if (this.user.hoursOfSport > 50) {
+      this.formValidation.messages.push(this.user.hoursOfSport + ' Stunden Sport pro Woche? ;-) Wirklich?' );
+      this.formValidation.valid = false;
+    }
+  }
+
+  private validateAge() {
+     let age = this.userService.calcAge(this.user.birthday);
+
+     if (this.user.birthday.length <= 0 || age > 120) {
+        this.formValidation.messages.push( 'Bitte gültiges Geburtsdatum angeben.' );
+        this.formValidation.valid = false;
+     } else if (age < 18) {
+       this.formValidation.messages.push( 'Zu jung... Leider können wir für unter 18-jährige keine zuverlässigen Berechnungen anbieten.' );
+       this.formValidation.valid = false;
+     }
   }
 
   saveUser() {
     if (this.validateForm()) {
       this.userService.updateUserInfo(this.user);
+      this.savedMessage = 'Angaben wurden gespeichert';
+    } else {
+      this.savedMessage = 'Nicht gespeichert, bitte Angaben prüfen.';
     }
+
+    let that = this;
+    setTimeout(function(){
+        that.savedMessage = '';
+     }, 3000);
+
   }
+
 }
