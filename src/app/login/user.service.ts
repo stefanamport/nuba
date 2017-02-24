26383fd1b11c4698cb995b44d6ab437b4b11186b
Stefan Amport <privat@stefanamport.ch>
2017-02-24 18:43:10 +0100

    age validation

--- src/app/login/user.service.ts
+++ src/app/login/user.service.ts
@@ -186,19 +186,29 @@ export class UserService {
   }
 
   private setAge() {
-    let age = 0;
 
-    if (this.userInfo.birthday) {
-      let birthday = new Date(this.userInfo.birthday);
+    let age = this.calcAge(this.userInfo.birthday);
+
+    this.userInfo.age = age;
+  }
+
+   public calcAge ( birthday: string ) {
+
+    let age = 0;
+    let inputbirthday = birthday;
+
+    if (inputbirthday) {
+      let birthdayDate = new Date( inputbirthday );
       let today = new Date();
 
-      age = today.getFullYear() - birthday.getFullYear();
-      let monthDiff = today.getMonth() - birthday.getMonth();
-      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthday.getDate())) {
+      age = today.getFullYear() - birthdayDate.getFullYear();
+      let monthDiff = today.getMonth() - birthdayDate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthdayDate.getDate())) {
           age = age - 1;
       }
     }
 
-    this.userInfo.age = age;
+    return age;
   }
+
 }
--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -14,8 +14,11 @@ export class UserAccountComponent  {
   user: User;
   genders: any;
   activityLevels: any;
+  formValidation: any = {};
 
   constructor (private userService: UserService) {
+    this.formValidation.valid = true;
+
     this.genders = Genders;
     this.activityLevels = ActivityLevels;
     this.user = this.userService.getUser();
@@ -30,7 +33,24 @@ export class UserAccountComponent  {
     return Object.keys(object);
   }
 
+  validateForm(): boolean {
+
+    this.formValidation.valid = true;
+
+     // check age
+     let age = this.userService.calcAge(this.user.birthday);
+
+     if (age < 18) {
+       this.formValidation.message = 'Zu jung... Leider können wir für unter 18-jährige keine zuverlässigen Berechnungen anbieten.';
+       this.formValidation.valid = false;
+     }
+
+     return this.formValidation.valid;
+  }
+
   saveUser() {
-    this.userService.updateUserInfo(this.user);
+    if (this.validateForm()) {
+      this.userService.updateUserInfo(this.user);
+    }
   }
 }
