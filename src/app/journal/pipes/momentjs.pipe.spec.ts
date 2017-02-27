c7bc5674d3c3f00a92774926954f183131d6550b
Stefan Amport <privat@stefanamport.ch>
2017-02-27 21:27:03 +0100

    added pipe test file

---
+++ src/app/journal/pipes/momentjs.pipe.spec.ts
@@ -0,0 +1,24 @@
+import { MomentPipe } from './momentjs.pipe';
+
+describe('MomentPipe', () => {
+
+    let pipe: MomentPipe;
+
+    let input = 'Mon Feb 27 2017 21:22:12 GMT+0100 (CET)' ;
+    let format = 'HH:mm YYYY Do MMMM';
+    let output = '21:22 2017 27. Februar';
+
+    console.log(new Date());
+
+    beforeEach(() => {
+        pipe = new MomentPipe();
+    });
+
+    it('should transform date', () => {
+
+        let transformedDate = pipe.transform(input, format);
+
+        expect(transformedDate).toEqual(output);
+    });
+
+});
