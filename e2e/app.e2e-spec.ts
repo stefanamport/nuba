825a00aa726f6e0caadec3027676ae0c5d61440b
Sonja Näf <sonjanaef@Sonjas-MacBook-Pro.local>
2017-03-15 00:45:53 +0100

    Re-arrange e2e test so that login is done first and then searched for food

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -20,59 +20,73 @@ describe('angular-cli-project App', function() {
     expect(elem.isPresent()).toBeTruthy();
   });
 
-  it('should do login', () => {
+  it('should do login and search for food', () => {
     page.navigateTo('login');
 
-    let button = element(by.buttonText('Login mit Google Konto'));
-    expect(button.isPresent()).toBeTruthy();
+    // login first
+    let loginExec = new LoginExecution();
+    loginExec.doLogin();
 
-    // click 'Login mit Google Konto' button
-    button.click();
-    browser.driver.sleep(2000);
+    // then search for food
+    let searchExec = new SearchExecution();
+    searchExec.searchFood();
+  });
 
-    browser.getAllWindowHandles().then(function (handles) {
-      // pop up opens to login with Google account
-      browser.switchTo().window(handles[1]);
-      browser.driver.sleep(2000);
-      expect(browser.getCurrentUrl()).toContain('https://accounts.google.com/ServiceLogin');
+  class LoginExecution {
 
-      // set email
-      element(by.css('#Email')).sendKeys('nubaawesometest@gmail.com');
+    doLogin() {
+      let button = element(by.buttonText('Login mit Google Konto'));
+      expect(button.isPresent()).toBeTruthy();
 
-      // click on next button
-      let nextButton = element(by.css('#next'));
-      nextButton.click();
+      // click 'Login mit Google Konto' button
+      button.click();
       browser.driver.sleep(2000);
 
-      // set password
-      element(by.css('#Passwd')).sendKeys('1nuba2awesome3test');
+      browser.getAllWindowHandles().then(function (handles) {
+        // pop up opens to login with Google account
+        browser.switchTo().window(handles[1]);
+        browser.driver.sleep(2000);
+        expect(browser.getCurrentUrl()).toContain('https://accounts.google.com/ServiceLogin');
 
-      // click signin button
-      element(by.css('#signIn')).click();
+        // set email
+        element(by.css('#Email')).sendKeys('nubaawesometest@gmail.com');
 
-      // wait 5 seconds while authentication takes place
-      browser.driver.sleep(5000);
+        // click on next button
+        let nextButton = element(by.css('#next'));
+        nextButton.click();
+        browser.driver.sleep(2000);
 
-      // switch back to nuba's window
-      browser.driver.switchTo().window(handles[0]);
+        // set password
+        element(by.css('#Passwd')).sendKeys('1nuba2awesome3test');
 
-      // check whether search input field is available after login
+        // click signin button
+        element(by.css('#signIn')).click();
+
+        // wait 5 seconds while authentication takes place
+        browser.driver.sleep(5000);
+
+        // switch back to nuba's window
+        browser.driver.switchTo().window(handles[0]);
+
+        // check whether search input field is available after login
+        let searchInput = element(by.css('.searchbar__maininput'));
+        expect(searchInput.isPresent()).toBeTruthy();
+      });
+    }
+  }
+
+  class SearchExecution {
+
+    searchFood() {
+      // search for 'Brot'
       let searchInput = element(by.css('.searchbar__maininput'));
-      expect(searchInput.isPresent()).toBeTruthy();
-    });
-  });
+      searchInput.sendKeys('Brot');
+      browser.driver.sleep(1000);
 
-  it('should search for food', () => {
-    // user is already logged in from previous test 'should do login'
-
-    // search for 'Brot'
-    let searchInput = element(by.css('.searchbar__maininput'));
-    searchInput.sendKeys('Brot');
-    browser.driver.sleep(1000);
-
-    // search term should be found and therefore result list must not be empty
-    let resultList = element.all(by.css('.resultlist__entry'));
-    let numberOfElements = resultList.count();
-    expect(numberOfElements).toBeGreaterThan(0);
-  });
+      // search term should be found and therefore result list must not be empty
+      let resultList = element.all(by.css('.resultlist__entry'));
+      let numberOfElements = resultList.count();
+      expect(numberOfElements).toBeGreaterThan(0);
+    }
+  }
 });
