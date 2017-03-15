cf55a5d2f904588f205ac012e57142d800873966
Sonja Näf <sonjanaef@Sonjas-MacBook-Pro.local>
2017-03-15 16:40:52 +0100

    Write tests for the journal component

--- src/app/journal/journal.component.spec.ts
+++ src/app/journal/journal.component.spec.ts
@@ -1,17 +1,48 @@
 import { JournalComponent } from './journal.component';
-import { AnalysisService } from '../analysis/service/analysis.service';
-import {DateChooserService} from '../shared/date-chooser.service';
+import { Observable } from 'rxjs';
+import { ConsumptionReport } from '../analysis/model/consumptionReport';
+
+class DateChooserServiceStub {
+  getChosenDateAsObservable(): Observable<Date> {
+    return Observable.of(new Date());
+  }
+}
+
+class AnalysisServiceStub {
+  initConsumptionAnalysis() { }
+
+  getConsumptionReport() {
+    return Observable.of(new ConsumptionReport());
+  }
+}
 
 describe('JournalComponent', () => {
   let component: JournalComponent;
-  let analysisService: AnalysisService;
-  let dateChooserService: DateChooserService;
+  let analysisService: any = new AnalysisServiceStub();
+  let dateChooserService: any = new DateChooserServiceStub();
 
   beforeEach(() => {
     component = new JournalComponent(analysisService, dateChooserService);
+    component.ngOnInit();
   });
 
   it('should create JournalComponent', () => {
    expect(component).toBeTruthy();
+  });
+
+  it('should switch new hint available', () => {
+    expect(component.newHintAvailable).toBeTruthy();
+
+    component.showNewHint();
+
+    expect(component.newHintAvailable).toBeFalsy();
+  });
+
+  it('should change coach visibility', () => {
+    expect(component.mobCoachVisible).toBeFalsy();
+
+    component.mobCoachVisibility();
+
+    expect(component.mobCoachVisible).toBeTruthy();
   });
 });
