df55dbf086e1873773d43e2e516955a0ca83bc2c
Stefan Amport <privat@stefanamport.ch>
2017-03-16 21:00:35 +0100

    fixed iphone display bug & added ios app functionality

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
 
-  constructor() {
+  constructor(private loginService: LoginService, private router: Router) { }
 
+  ngOnInit() {
+    let user = this.loginService.getUser();
+    console.log(user);
+    if (user.uid) {
+      this.router.navigate(['journal']);
+    }
   }
 
 }
