5cc471c8b2320ff3bbf8c469ea792bb6e3fbc091
Stefan Amport <privat@stefanamport.ch>
2017-03-18 18:25:59 +0100

    fixed tests

--- src/app/firebase/firebase.service.ts
+++ src/app/firebase/firebase.service.ts
@@ -7,7 +7,7 @@ import { Reginfo } from '../login/reginfo';
 @Injectable()
 export class FirebaseService {
 
-  constructor(private af: AngularFire) { console.log(af) }
+  constructor(private af: AngularFire) { console.log(af); }
 
   getList(resource: string): FirebaseListObservable<any[]> {
     return this.af.database.list(resource);
