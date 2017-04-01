b08349d1beff297317b2685612d8f5c439a551d4
Stefan Amport <privat@stefanamport.ch>
2017-04-01 14:29:18 +0200

    added redirect to journal, if user is logged in

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
+import {Router} from '@angular/router';
+import { LoginService } from '../login/login.service';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -7,9 +10,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
+
+    this.loginService.getUserAsObservable().subscribe((user) => {
+
+    	// Redirect if user is logged in
+        if (user.uid && this.router.url === '/welcome') {
+          this.router.navigate(['journal']);
+        }
+
+    });
 
   }
 
